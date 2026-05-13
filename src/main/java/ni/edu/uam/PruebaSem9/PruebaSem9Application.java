e30c603037174f9d0c38ca278a00894e305100ec
Fergago20 <fergomez@uamv.edu.ni>
2026-05-13 17:18:43 -0600

    Feat: Crear Proyecto

---
+++ src/main/java/ni/edu/uam/PruebaSem9/PruebaSem9Application.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.PruebaSem9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSem9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSem9Application.class, args);
+	}
+
+}
---
+++ src/test/java/ni/edu/uam/PruebaSem9/PruebaSem9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.PruebaSem9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSem9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
