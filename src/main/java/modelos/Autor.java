a43d9fa3226813611e3124c891fa2b34ae428b83
Fergago20 <fergomez@uamv.edu.ni>
2026-05-13 17:30:42 -0600

    Feat: Clases Bases Creadas

---
+++ src/main/java/modelos/Autor.java
@@ -0,0 +1,34 @@
+package modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "autores")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_autor", nullable = false)
+    private String id;
+
+    @Column(name = "nombre_autor", nullable = false, length = 100, unique = true)
+    private String nombre;
+
+    @Column(name = "fecha_nacimiento_autor", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Transient
+    @AssertTrue(message = "El autor debe ser mayor de edad")
+    public boolean mayorDeEdad() {
+        return fechaNacimiento != null && fechaNacimiento.isBefore(LocalDate.now().minusYears(18));
+    }
+}
---
+++ src/main/java/modelos/Libro.java
@@ -0,0 +1,40 @@
+package modelos;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.AssertTrue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "libros")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_libro", nullable = false)
+    private String id;
+
+    @Column(name = "titulo_libro", nullable = false, length = 200)
+    private String titulo;
+
+    @Column(name = "fecha_publicacion_libro", nullable = false)
+    private LocalDate fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_autor", nullable = false)
+    private Autor autor;
+
+    @Transient
+    @AssertTrue(message = "La fecha de publicación no puede ser futura")
+    public boolean fechaValida() {
+        return fecha != null && !fecha.isAfter(LocalDate.now());
+    }
+}
